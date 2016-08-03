16b3f759591ff142495a1faf533870bee49dfdaa
TomokoKondo <to.2a721@gmail.com>
2016-08-04 03:27:20 +0900

    シーン１をちょっとだけ作った

---
+++ Unity/cafeniwa/Assets/Scripts/CafeMain/TapAbleObject.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TapAbleObject : MonoBehaviour {
+
+	public GameObject dialog;
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown(0)) {
+
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			RaycastHit hit = new RaycastHit();
+
+			if (Physics.Raycast(ray, out hit)){
+				dialog.SetActive (true);
+			}
+		}
+	}
+}
